bb85a3eec5d96df0a18abb820a464ac344aefc75
jllord <jessica@codeforamerica.org>
2013-03-29 22:05:53 -0700

    all the filez

---
+++ proxy.js
@@ -0,0 +1,16 @@
+// Dependencies 
+var http 		= require('http');
+    request = require('request');
+
+// Talk to another server
+function proxyHandler(req, res) {
+	var reqOptions = {
+		url: 'https://brain.diy.org/makers/goldenrod/stream',
+		headers: {"x-diy-api-key" : "DIY_MAKER_KEY"}
+	}
+	req.pipe(request(reqOptions)).pipe(res);
+}
+
+// Create a server, use a proxy
+http.createServer(proxyHandler).listen(8080);
+console.log('Listening on port 8080');
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Proxy Server
+
+A simple proxy server that requests a DIY Maker's Stream data.
+
+Apart of the [DIY Backend Dev Skill](http://www.diy.org/skills/backenddev). 
